d26bcd9134fcd1e9f0c13278e2e7a3a7c42399ce
sk790 <saurabhk2890@gmail.com>
2024-08-03 10:48:23 +0530

    created a navbar and a sub sidebar

---
+++ app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex items-center gap-x-4 p-5 bg-green-500'>
+        <div className='hidden lg:flex lg:flex-1'>
+            Search
+        </div>
+        <UserButton/>
+    </div>
+  )
+}
+
+export default Navbar
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Sidebar from './_components/sidebar/Sidebar';
+import React from "react";
+import Sidebar from "./_components/sidebar/Sidebar";
+import OrgSidebar from "./_components/OrgSidebar";
+import Navbar from "./_components/Navbar";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <main className="h-full">
-        <Sidebar />
-        {children}
-      </main>
-    );
-  }
-  
-  export default DashboardLayout;
-  
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default DashboardLayout;
