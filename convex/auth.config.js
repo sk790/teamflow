8e3af31c2bd9d8ee0d59cac8d0eee3d1a9b8ad48
sk790 <saurabhk2890@gmail.com>
2024-08-03 10:38:48 +0530

    interate the user authentication system for authenticated user and convex authentication with the help of convexclientprovider

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://outgoing-herring-58.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/convex-clerk-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Loading from "@/components/auth/Loading";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
