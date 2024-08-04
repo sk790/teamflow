8c821637a68033751d77f082e69694c55a03ada9
sk790 <saurabhk2890@gmail.com>
2024-08-04 22:00:39 +0530

    created update board api

--- convex/board.ts
+++ convex/board.ts
@@ -42,3 +42,28 @@ export const deleteBoard = mutation({
     await ctx.db.delete(args.id);
   },
 });
+
+export const updateBoard = mutation({
+  args: {
+    id: v.id("boards"), //inside the boards table name
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new Error("Unauthorized");
+    }
+    const tittle = args.title.trim();
+    if (!tittle) {
+      throw new Error("Title cannot be empty");
+    }
+    if (tittle.length > 50 || tittle.length < 2) {
+      throw new Error(
+        "Title cannot be longer than 50 characters or shorter than 2"
+      );
+    }
+    return await ctx.db.patch(args.id, {
+      title: args.title,
+    });
+  },
+});
