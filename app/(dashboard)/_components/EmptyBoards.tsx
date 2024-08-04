5121cd7d400693dbec721a2ab6e21acba30e48bc
sk790 <saurabhk2890@gmail.com>
2024-08-04 11:19:42 +0530

    created a convex api for creating a board

--- app/(dashboard)/_components/EmptyBoards.tsx
+++ app/(dashboard)/_components/EmptyBoards.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
 import Image from "next/image";
 import React from "react";
 
+
 function EmptyBoards() {
+  const {organization} = useOrganization()
+  const create = useMutation(api.board.create)
+
+  const onClick = ()=>{
+    if(!organization) return
+    create({
+      orgId: organization.id,
+      title: "New Board",
+    })
+  }
+
+
   return (
     <div className="w-full h-full flex justify-center items-center flex-col">
       <Image
@@ -14,7 +32,7 @@ function EmptyBoards() {
       <h2 className="font-semibold mt-4 text-2xl">Create your first board</h2>
       <p className="text-muted-foreground mt-2 text-sm"> Start by creating a new board for your team</p>
       <div className="mt-6">
-        <Button size={"lg"}>Create Board</Button>
+        <Button onClick={onClick} size={"lg"}>Create Board</Button>
       </div>
     </div>
   );
---
+++ convex/board.ts
@@ -0,0 +1,32 @@
+import { v } from "convex/values"
+import { mutation} from "./_generated/server" 
+
+
+const images = [
+    "/search-not-found.svg",    //add more images here later
+]
+export const create = mutation({
+    args:{
+        orgId:v.string(),
+        title:v.string(),
+    },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity();
+        if(!user){
+            throw new Error("Unauthorized");
+        }
+
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+
+        const board = await ctx.db.insert("boards",{
+            orgId:args.orgId,
+            title:args.title,
+            authorId:user.subject,
+            authorName:user.name!,
+            imageUrl:randomImage,
+        })
+
+        return board;
+
+    }
+})
