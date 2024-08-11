aff51aa140e4a8539cf1f0a7d0c555b78ccc8fca
sk790 <saurabhk2890@gmail.com>
2024-08-11 13:39:49 +0530

    completed all functionality of canvas

--- app/board/[boardId]/_components/CursorsPresence.tsx
+++ app/board/[boardId]/_components/CursorsPresence.tsx
@@ -1,20 +1,60 @@
-import { useOthersConnectionIds } from "@liveblocks/react";
+import {
+  shallow,
+  useOthersConnectionIds,
+  useOthersMapped,
+} from "@liveblocks/react";
 import React, { memo } from "react";
 import { Cursor } from "./Cursor";
+import { Path } from "./Path";
+import { ColorToCss } from "@/lib/utils";
+
+const Cursors = () => {
+  const ids = useOthersConnectionIds();
+  return (
+    <>
+      {ids.map((connectionId) => (
+        <Cursor key={connectionId} connectionId={connectionId} />
+      ))}
+    </>
+  );
+};
+
+const Draft = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? ColorToCss(other.penColor) : "#000"}
+            />
+          );
+        }
+        return null;
+      })}
+    </>
+  );
+};
 
 export const CursorsPresence = memo(() => {
-  const Cursors = () => {
-    const ids = useOthersConnectionIds();
-    return (
-      <>
-        {ids.map((connectionId) => (
-          <Cursor key={connectionId} connectionId={connectionId} />
-        ))}
-      </>
-    );
-  };
-
-  return <Cursors />;
+  return (
+    <>
+      <Draft />
+      <Cursors />
+    </>
+  );
 });
 
 CursorsPresence.displayName = "CursorsPresence";
